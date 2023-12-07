87ca87faee856a85fe2a2777341b82ed605fba08
lilucool <lilucool@users.noreply.github.com>
2023-12-07 10:54:06 +0800

    add mall

--- WS20231102/index.js
+++ WS20231102/index.js
@@ -13,28 +13,93 @@ const wsServer = new websocketServe({
 
 
 wsServer.on("request", request => {
+
     const connection = request.accept(null, request.origin);
-   // connection.on("open", () => console.log("opened!"))
-   // connection.on("close", () => console.log("closed!"))
-  connection.on("message", message => {
-        //
-        //
-
-    const result = JSON.parse(message.utf8Data)
-     console.log(result)
-  })
-
     const clientId = guid();
+    const games = {};
+    const clients = {};
+    console.log("ÇëÇóÊÇ£º" + request);
+
     clients[clientId] = {
         "connection": connection
     }
 
     const payLoad = {
-       "method": "connect",
-        "clientId":clientId
+        "method": "connect",
+        "clientId": clientId
     }
+    connection.on("open", () => console.log("opened!" + connection.state))
+    connection.on("close", () => console.log("closed!" + connection.state))
+    connection.on("message", message => {
+        //
+        //
+
+        const result = JSON.parse(message.utf8Data)
+        console.log(result)
+
+       
+
+       
+
+
+        //a user want to create a new game
+        if (result.method === "create") {
+            const clientId = result.clientId;
+            const gameId = guid();
+            games[gameId] = {
+                "id": gameId,
+                "balls": 20,
+                "clients": []
+            }
+
+            const payLoad = {
+                "method": "create",
+                "game": games[gameId]
+            }
+
+            const con = clients[clientId].connection;
+            con.send(JSON.stringify(payLoad));
+        }
+
+
+
+        //a client want to join
+        if (result.method === "join") {
+
+            const clientId = result.clientId;
+            const gameId = result.gameId;
+            const game = games[gameId];
+            if (game.clients.length >= 3) {
+                //sorry max players reach
+                return;
+            }
+            const color = { "0": "Red", "1": "Green", "2": "Blue" }[game.clients.length]
+            game.clients.push({
+                "clientId": clientId,
+                "color": color
+            })
+            //start the game
+            if (game.clients.length === 3) updateGameState();
+
+            const payLoad = {
+                "method": "join",
+                "game": game
+            }
+            //loop through all clients and tell them that people has joined
+            game.clients.forEach(c => {
+                clients[c.clientId].connection.send(JSON.stringify(payLoad))
+            })
+        }
+
+  })
+
+
+
+
+
 
     connection.send(JSON.stringify(payLoad))
+
 
 
 })
